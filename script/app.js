04a59c10376f66ae416aa505d48718300b00b534
jami <adam.jami@etu.univ-paris-diderot.fr>
2024-03-07 15:48:03 +0100

    update vue

--- script/app.js
+++ script/app.js
@@ -1,1 +1,37 @@
 
+var socket = io();
+socket.on('connect' , () => { console.log(socket.id)});
+
+
+
+
+var app = new Vue({
+
+    el: '#app',
+
+    data: function() {
+        return {
+            testo:"pikinemad"
+        }
+    },
+
+
+    methods: {
+
+        
+
+    },
+
+    created:  function() {
+        
+
+    },
+
+   
+    mounted: async function() {
+        
+        
+
+    },
+
+})
